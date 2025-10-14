fe6eabe8f6566b6ea7ef865ee930fe446510fa87
rambip <apero1808@gmail.com>
2025-10-14 07:26:36 +0200

    improve build pipeline

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-DEVICE="fr165"
-monkeyc -y developer_key.der -d $DEVICE -f monkey.jungle -o result.prg --release
+
+# List of devices to build for
+DEVICES="fr165 fr245"
+
+# Loop through each device
+for DEVICE in $DEVICES; do
+    echo "Building for device: $DEVICE"
+    monkeyc -y developer_key.der -d $DEVICE -f monkey.jungle -o "build/reminder_app_${DEVICE}.prg" --release
+done
+
+echo "Build completed for all devices"
